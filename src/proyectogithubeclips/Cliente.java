31eab4d9f845cfd31f1a537fcba5a0ef3acb49be
raquel <richo181095@gmail.com>
2024-04-14 18:57:08 +0200

    Añadido constructor de Cliente

---
+++ src/proyectogithubeclips/Cliente.java
@@ -0,0 +1,29 @@
+package proyectogithubeclips;
+
+public class Cliente {
+	
+	String nombre;
+	String apellido;
+	String dni;
+	
+	public Cliente(String nombre, String apellido, String dni) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
