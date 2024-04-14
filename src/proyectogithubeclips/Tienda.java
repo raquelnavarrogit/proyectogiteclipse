1fd15bf0361d274d21904f0c81531145db8b4013
raquel <richo181095@gmail.com>
2024-04-14 18:52:09 +0200

    Añadido atributo de clase Producto

---
+++ src/proyectogithubeclips/Tienda.java
@@ -0,0 +1,11 @@
+package proyectogithubeclips;
+
+import java.util.ArrayList;
+
+public class Tienda {
+
+	ArrayList <Cliente> usuarios = new ArrayList <Cliente> ();
+	ArrayList <Producto> stock = new ArrayList <Producto> ();
+	Cliente cliente;
+	Producto producto;
+}
