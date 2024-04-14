7b6d469da2b7c26b326d2065201fdf7079ec1018
raquel <richo181095@gmail.com>
2024-04-14 18:47:48 +0200

    Añadido constructor

--- src/proyectogithubeclips/Producto.java
+++ src/proyectogithubeclips/Producto.java
@@ -7,5 +7,22 @@ public class Producto {
 	private String marca ;
 	private int cantidad;
 	
+	public Producto(int id, String nombre, String marca, int cantidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.marca = marca;
+		this.cantidad = cantidad;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
