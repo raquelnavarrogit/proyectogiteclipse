2eec2fd5bf14280f816839350ebbbde3033284f8
raquel <richo181095@gmail.com>
2024-04-14 18:59:59 +0200

    Añadido método toString

--- src/proyectogithubeclips/Cliente.java
+++ src/proyectogithubeclips/Cliente.java
@@ -37,6 +37,10 @@ public class Cliente {
 		this.dni = dni;
 	}
 	
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + "]";
+	}
 	
 	
 	
