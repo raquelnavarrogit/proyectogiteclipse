d0190731d321005387857d2b985e2366aab825f6
raquel <richo181095@gmail.com>
2024-04-14 18:54:18 +0200

    Añadido método añadir cliente

--- src/proyectogithubeclips/Tienda.java
+++ src/proyectogithubeclips/Tienda.java
@@ -2,6 +2,8 @@ package proyectogithubeclips;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Tienda {
 
 	ArrayList <Cliente> usuarios = new ArrayList <Cliente> ();
@@ -13,6 +15,16 @@ public class Tienda {
 		super();
 	}
 	
+	public void añadirCliente () {
+		
+		String nom = JOptionPane.showInputDialog("Introduzca el nombre del cliente: ");
+		String ape = JOptionPane.showInputDialog("Introduzca el apellido del cliente: ");
+		String dni = JOptionPane.showInputDialog("Introduzca el dni del cliente: ");
+		
+		Cliente usuario = new Cliente (nom,ape,dni);
+		
+		usuarios.add(usuario);
+	}
 	
 	
 	
