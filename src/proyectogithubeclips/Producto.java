a19687871213c188e300b4e5a7b96b844c26ed80
raquel <richo181095@gmail.com>
2024-04-14 18:49:32 +0200

    Añadido método toString

--- src/proyectogithubeclips/Producto.java
+++ src/proyectogithubeclips/Producto.java
@@ -15,6 +15,11 @@ public class Producto {
 		this.cantidad = cantidad;
 	}
 
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", marca=" + marca + ", cantidad=" + cantidad + "]";
+	}
+	
 	
 	
 	
